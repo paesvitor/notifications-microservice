710752419cf178845fd3a55476ff3abe012c22a4
Vitor Paes <paesvitor@outlook.com>
2024-09-25 00:43:46 -0300

    Connect prisma infra

---
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -0,0 +1,20 @@
+import type { Notification } from 'src/app/entities/notification';
+import type { NotificationsRepository } from '../../../../app/repositories/notifications-repository';
+import type { PrismaService } from '../prisma.service';
+
+export class PrismaNotificationsRepository implements NotificationsRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(notification: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        id: notification.id,
+        content: notification.content.value,
+        category: notification.category,
+        recipientId: notification.recipientId,
+        readAt: notification.readAt,
+        createdAt: notification.createdAt,
+      },
+    });
+  }
+}
