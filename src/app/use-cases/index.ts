2a32d6922378b2b3b81ed58e45551c535ee0febd
Vitor Paes <paesvitor@outlook.com>
2024-10-03 01:38:46 -0300

    Implement routes

---
+++ src/app/use-cases/index.ts
@@ -0,0 +1,6 @@
+export * from './cancel-notification';
+export * from './count-recipient-notifications';
+export * from './get-recipient-notifications';
+export * from './read-notification';
+export * from './send-notification';
+export * from './unread-notification';
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,12 +5,14 @@ import { PrismaService } from '../database/prisma/prisma.service';
 import { NotificationsRepository } from 'src/app/repositories/notifications-repository';
 import { PrismaNotificationsRepository } from '../database/prisma/repositories/prisma-notifications-repository';
 import { SendNotification } from 'src/app/use-cases/send-notification';
+import { CancelNotification } from 'src/app/use-cases/cancel-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
   providers: [
     SendNotification,
+    CancelNotification,
     PrismaService,
     {
       provide: NotificationsRepository,
